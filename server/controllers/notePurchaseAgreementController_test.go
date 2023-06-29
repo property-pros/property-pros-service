8fe186060c5dc7e013bbafc07007931e78b00f57
Scott Terry <srt0422@gmail.com>
2023-06-29 12:06:09 -0700

    revert update to GetStatements

--- server/controllers/notePurchaseAgreementController_test.go
+++ server/controllers/notePurchaseAgreementController_test.go
@@ -48,3 +48,53 @@ func TestNotePurchaseAgreementController_EndToEnd(t *testing.T) {
         t.Errorf("Saved and retrieved note purchase agreements do not match.")
     }
 }
+
+// Test GetNotePurchaseAgreementDoc
+func TestNotePurchaseAgreementController_GetNotePurchaseAgreementDoc(t *testing.T) {
+    // Set up a connection to the gRPC server
+    conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+    if err != nil {
+        t.Fatalf("Failed to connect to gRPC server: %v", err)
+    }
+    defer conn.Close()
+
+    // Create a client for the NotePurchaseAgreementService
+    client := interop.NewNotePurchaseAgreementServiceClient(conn)
+
+    // Create test data
+    testNotePurchaseAgreementDoc := &interop.NotePurchaseAgreementDoc{
+        Id:        "1",
+        CreatedOn: "2020-01-01T00:00:00Z",
+        FileContent: []byte("test file content"),
+    }
+
+    // Save a new note purchase agreement doc
+    ctx := context.WithValue(context.Background(), constants.UserIdKey, "1")
+    saveRes, err := client.SaveNotePurchaseAgreementDoc(ctx, &interop.SaveNotePurchaseAgreementDocRequest{
+        Payload: testNotePurchaseAgreementDoc,
+    })
+    if err != nil {
+        t.Fatalf("Failed to save note purchase agreement doc: %v", err)
+    }
+	
+    // Get the newly saved note purchase agreement doc
+    getRes, err := client.GetNotePurchaseAgreementDoc(ctx, &interop.GetNotePurchaseAgreementDocRequest{
+        Payload: saveRes.GetPayload().GetId(),
+    })
+    if err != nil {
+        t.Fatalf("Failed to get note purchase agreement doc: %v", err)
+    }
+
+    // Assert the saved and retrieved note purchase agreement docs match
+    if getRes.GetPayload().GetId() != testNotePurchaseAgreementDoc.GetId() {
+        t.Errorf("Saved and retrieved note purchase agreement docs do not match.")
+    }
+}
+
+
+
+// Test GetNotePurchaseAgreements
+func TestNotePurchaseAgreementController_GetNotePurchaseAgreements(t *testing.T) {
+    // ... 
+}
+
